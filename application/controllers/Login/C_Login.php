b1e8345825a015904700a747b22a90f6d0106357
Jose Narvaez <jose.narvaez@sonovista.co>
2020-05-22 14:44:56 -0500

    Recuperar password

--- application/controllers/Login/C_Login.php
+++ application/controllers/Login/C_Login.php
@@ -35,5 +35,23 @@ class C_Login extends Controller {
             echo "nada papá";
         }
     }
+    
+    function ForgotToPassword(){
+        $emailTo = $this->input->post("email");
+        
+        $validate = $this->M_Main->ValidateForgot($emailTo);
+
+        if($validate > 0){
+            $cc = "";
+            $subject = "Recuperar contraseña";
+            $html = $this->load->view("Login/V_Email_Template",array("id"=>$validate),true);
+//            $html = "<h2>Recuperar Contraseña</h2><hr><br> <a href='".base_url()."C_Service/ForgotToPassword/".$validate."'>Clic AQUI<a>";
+
+            $res = $this->SendEmail($emailTo,$cc,$subject,$html);
+        }else{
+            $res = "EMPTY";
+        }
+        echo $res;     
+    }
    
 }
